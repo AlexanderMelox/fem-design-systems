481e9187513e33d583a030fc90baea1fae2d7725
Alexander Melo <alexandermelox@gmail.com>
2020-03-17 11:15:17 -0500

    built storybook

---
+++ storybook-static/precache-manifest.215f1393925ab40e4441f8e696d7d9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75e0f080a2b84ff63398ca7933d50fe",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e08ac977518da252b854.bundle.js"
+  },
+  {
+    "url": "runtime~main.e08ac977518da252b854.bundle.js"
+  },
+  {
+    "revision": "77b4ee166ccdcf8134673d7a35822098",
+    "url": "static/media/SignIn.77b4ee16.svg"
+  },
+  {
+    "revision": "d9f4c005d39b8256e77d068502aa2bd4",
+    "url": "static/media/SignUp.d9f4c005.svg"
+  },
+  {
+    "url": "vendors~main.e08ac977518da252b854.bundle.js"
+  },
+  {
+    "url": "vendors~main.e08ac977518da252b854.bundle.js.LICENSE.txt"
+  }
+]);
